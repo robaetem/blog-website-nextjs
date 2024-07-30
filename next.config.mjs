03a86a380d04858bddc6467e618349284e2c375f
Robin Baeteman <robin.baeteman@ae.be>
2024-07-30 11:56:33 +0200

    remove contentlayer again

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  output: "export",
-  images: {unoptimized: true}
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx']
 }
 
 const withMDX = nextMDX({
